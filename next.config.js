39f10abddbece4f6c6e8ba444e9e08d6c24b83c0
PBillingsby <philbillingsby@gmail.com>
2022-08-30 11:44:00 -0400

    add webpack config to next config

--- next.config.js
+++ next.config.js
@@ -3,15 +3,11 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
     if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
+      config.resolve.fallback.fs = false;
     }
-
-    return config
-  }
+    return config;
+  },
 }
 
 module.exports = nextConfig
